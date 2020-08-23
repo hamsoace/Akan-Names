24fbf81851586b38e74492cffc5d69f8e6322ea9
hamsoace <hamsoace@gmail.com>
2020-08-23 15:34:45 +0300

    corrected some variables

--- js/app.js
+++ js/app.js
@@ -1,24 +1,34 @@
 function myAkanName(){
-    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    let maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-    let femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-    let birthDay = document.getElementById("datepicker").value;
-    let gender = document.getElementsByName("gender") 
-    let dateOfBirth = new Date(birthday);
-    let dayOfTheWeek = dateOfBirth.getDay();
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var birthDay = document.getElementById("datepicker").value;
+    var myGender = document.getElementsByName("gender"); 
+    var dateOfBirth = new Date(birthDay);
+    var dayOfTheWeek = dateOfBirth.getDay();
+    
 
-    if(birthday === "" && gender === "--"){
-        alert("Fill in the date")
+    if(birthDay === ""){
+        alert("Fill in the date");
     }
-    else if{
-        for (let i = 0; i < gender.length; i++) {
-            if(gender[i].checked){
-                if(gender[i].value === "Male"){
-                    alert("You were born on " +days[dayOfTheWeek] + " and Your akan name is " +maleAkanNames[dayOfTheWeek]+"!");
+    else {
+            for (var i = 0; i < myGender.length; i++) {
+                if(myGender[i].checked){
+                    if(myGender[i].value === "male"){
+                        alert("You were born on " +days[dayOfTheWeek] + " and Your akan name is " +maleAkanNames[dayOfTheWeek]+"!");
+                    }
+                    else{
+                        alert("You were born on " +days[dayOfTheWeek] + " and Your Akan Name is " +femaleAkanNames[dayOfTheWeek]+"!");
+                    }
                 }
+                
             }
-            
         }
-    }
+
+}
+
+
+function clearAkanMessage(){
+    document.getElementById('message').innerHTML = "";
 }
 
